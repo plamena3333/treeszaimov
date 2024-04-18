b8249fd962ecf145b150dfed44427db5089bdb5f
plamena333 <plamena.zheleva643@edu1.pgzaimov.com>
2024-04-18 10:34:40 +0300

    Trees

---
+++ TreesZaimov/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TreesZaimov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNameBG = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(364, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INSERT INTO otdel tabler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NameBG";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(11, 95);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtNameBG
+            // 
+            this.txtNameBG.Location = new System.Drawing.Point(225, 95);
+            this.txtNameBG.Name = "txtNameBG";
+            this.txtNameBG.Size = new System.Drawing.Size(160, 20);
+            this.txtNameBG.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 266);
+            this.Controls.Add(this.txtNameBG);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "pomoshtnitablici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNameBG;
+    }
+}
+
---
+++ TreesZaimov/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreesZaimov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conectionString = "server=192.168.1.38;" +
+            "user=PC1;" +
+            "password=1111;" +
+            "port=3306;" +
+            "database = trees_zaimov";
+
+        MySqlConnection connect = new MySqlConnection(conectionString);
+            if (connect.State == 0)
+            {
+                connect.Open();
+            }
+            MessageBox.Show("Imash vruzka s HeidiSQL");
+
+            //formirane na query
+
+            string insertQueryText = "INSERT INTO trees_zaimov.otdel" +
+                   "(`name` , `name_bg`) " +
+                   "VALUES " +
+                   "(@name,@name_bg)";
+
+            MySqlCommand query = new MySqlCommand(insertQueryText, connect);
+
+            //svurzvane na parametri
+            query.Parameters.AddWithValue("@name", txtName.Text);
+            query.Parameters.AddWithValue("@name_bg", txtNameBG.Text);
+            query.ExecuteNonQuery();
+           
+            //Run query
+            int result = query.ExecuteNonQuery();
+            if (result != 0)
+            {
+                MessageBox.Show($"Dobavi {result} records!!!");
+            }
+
+            connect.Close();
+
+
+        }
+    }
+}
